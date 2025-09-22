6c8a8d3f166b2eef9adba260535fc931870a14fd
NamHoang <hoangducnam.work@gmail.com>
2025-09-22 13:09:05 -0500

    fix padding

--- src/pages/findnanny/Onboarding.jsx
+++ src/pages/findnanny/Onboarding.jsx
@@ -1,6 +1,6 @@
 const Onboarding = () => {
   return (
-    <div style={{ padding: '2rem', textAlign: 'center' }}>
+    <div style={{ padding: '8rem', textAlign: 'center' }}>
       <h1>Onboarding Process</h1>
       <p>This page is under construction. The onboarding process will be implemented in a future update.</p>
     </div>
